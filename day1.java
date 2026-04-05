47056198983dfb13f0a93ac0429b2eec1b6fb2b2
Kashyapxx <142075337+Kashyapxx@users.noreply.github.com>
2026-04-06 01:39:21 +0530

    day1.java
    Input/output basic code in java

---
+++ day1.java
@@ -0,0 +1,11 @@
+// INPUT/OUTPUT SIMPLE CODE
+import java.util.*; 
+
+public class day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        System.out.println("The number entered is ="+a);
+    }
+}
