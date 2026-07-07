a338f2c8c545bb88161c011688667f6769f19ca9
Kashyapxx <142075337+Kashyapxx@users.noreply.github.com>
2026-07-08 01:15:28 +0530

    day5.java

---
+++ day5.java
@@ -0,0 +1,10 @@
+//Basic code for finding the length of a string using lenth function
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string youy wanna know length for");
+        String str=sc.nextLine();
+        System.out.println("The length of the string is :-"+ str.length());
+    }
+}
